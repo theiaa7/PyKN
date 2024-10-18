f039b8da870cce0bd223f7e16b30e781aac3ed70
theiaa7 <ryan.alsty@gmail.com>
2024-10-18 18:29:05 +0700

    new lesson update

---
+++ L2 Python I/2. Comparison.py
@@ -0,0 +1,13 @@
+num1 = 7
+num2 = 7
+
+print(f"is {num1} equal to {num2} : {num1 == num2}")
+print(f"is {num1} not equal to {num2} : {num1 != num2}")
+print(f"is {num1} greater than {num2} : {num1 > num2}")
+print(f"is {num1} less than {num2} : {num1 < num2}")
+print(f"is {num1} greater or equal to {num2} : {num1 >= num2}")
+print(f"is {num1} less or equal to {num2} : {num1 <= num2}")
+
+age = 17
+print(age > 16)
+print(age >= 17)
